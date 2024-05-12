cac96c4501a3b8edcdcd2afbe9fcd9ffe1078838
Gazi Akter <gaziakterdev@gmail.com>
2024-05-12 20:01:37 +0600

    use condition functionality

--- widgets/icon-box.php
+++ widgets/icon-box.php
@@ -38,7 +38,7 @@ class Persona_Icon_box_Widget extends Widget_Base {
 	 * @return string Widget title.
 	 */
 	public function get_title() {
-		return __( 'Icon Box', 'persona' );
+		return __( 'Persona Icon Box', 'persona' );
 	}
 
 	/**
@@ -99,7 +99,7 @@ class Persona_Icon_box_Widget extends Widget_Base {
 	protected function register_controls() {
 
 		$this->start_controls_section(
-			'persoan_title_section',
+			'persona_icon_box_section',
 			[
 				'label' => esc_html__( 'Title and Content', 'persona-core' ),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
@@ -107,117 +107,41 @@ class Persona_Icon_box_Widget extends Widget_Base {
 		);
 
 		$this->add_control(
-			'hero_sub_title',
+			'persona_sub_title',
 			[
 				'label' => esc_html__( 'Sub Title', 'persona-core' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Hy ! I am Brian miller', 'persona-core' ),
+				'default' => esc_html__( '86 Projects', 'persona-core' ),
 				'placeholder' => esc_html__( 'Type your sub title here', 'persona-core' ),
 				'label_block' => true,
 			]
 		);
 
 		$this->add_control(
-			'hero_title',
+			'persona_title',
 			[
 				'label' => esc_html__( 'Title', 'persona-core' ),
 				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'Creative UI/UX designer', 'persona-core' ),
-				'placeholder' => esc_html__( 'Type your title here', 'persona-core' ),
+				'default' => esc_html__( 'Website Design', 'persona-core' ),
+				'placeholder' => esc_html__( 'Type your sub title here', 'persona-core' ),
 				'label_block' => true,
 			]
 		);
 
 		$this->add_control(
-			'hero_text',
+			'persona_select_icon',
 			[
-				'label' => esc_html__( 'Description', 'persona-core' ),
-				'type' => \Elementor\Controls_Manager::TEXTAREA,
-				'default' => esc_html__( 'Hi! I am a UI/UX Designer - creating bold & brave interface design for companies all across the world.', 'persona-core' ),
-				'placeholder' => esc_html__( 'Type your content here', 'persona-core' ),
-			]
-		);
-		
-
-		$this->end_controls_section();
-
-
-		/** Button Section */
-		$this->start_controls_section(
-			'persoan_button_section',
-			[
-				'label' => esc_html__( 'Button', 'persona-core' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-			]
-		);
-
-		$this->add_control(
-			'hero_button_text',
-			[
-				'label' => esc_html__( 'Button Text', 'persona-core' ),
-				'type' => \Elementor\Controls_Manager::TEXT,
-				'default' => esc_html__( 'book a call', 'persona-core' ),
-				'placeholder' => esc_html__( 'Type your button text here', 'persona-core' ),
-				'label_block' => true,
-			]
-		);
-
-		$this->add_control(
-			'hero_button_url',
-			[
-				'label' => esc_html__( 'Button URL', 'persona-core' ),
-				'type' => \Elementor\Controls_Manager::URL,
-				'options' => [ 'url', 'is_external', 'nofollow' ],
-				'default' => [
-					'url' => '',
-					'is_external' => true,
-					'nofollow' => true,
-					// 'custom_attributes' => '',
-				],
-				'label_block' => true,
-			]
-		);
-
-
-		$this->end_controls_section();
-
-
-
-		/** Image Section */
-		$this->start_controls_section(
-			'persoan_hero_img_section',
-			[
-				'label' => esc_html__( 'Hero Image', 'persona-core' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-			]
-		);
-
-		$this->add_control(
-			'hero_image',
-			[
-				'label' => esc_html__( 'Choose Image', 'persona-core' ),
-				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				'label' => esc_html__( 'Choose Icon Method', 'persona-core' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'default' => 'svg',
+				'options' => [
+					'icon' => esc_html__( 'Icon', 'persona-core' ),
+					'svg'  => esc_html__( 'SVG', 'persona-core' ),
 				],
 			]
 		);
 
-		$this->end_controls_section();
-
-
-		/** Social Section */
-		$this->start_controls_section(
-			'persoan_hero_img_social',
-			[
-				'label' => esc_html__( 'Social', 'persona-core' ),
-				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
-			]
-		);
-
-		$repeater = new \Elementor\Repeater();
-
-		$repeater->add_control(
+		$this->add_control(
 			'icon',
 			[
 				'label' => esc_html__( 'Icon', 'persona-core' ),
@@ -238,10 +162,27 @@ class Persona_Icon_box_Widget extends Widget_Base {
 						'square-full',
 					],
 				],
+				'condition' => [
+					'persona_select_icon' => 'icon',
+				],
 			]
 		);
 
-		$repeater->add_control(
+		$this->add_control(
+			'persona_svg_icon',
+			[
+				'label' => esc_html__( 'SVG', 'persona-core' ),
+				'type' => \Elementor\Controls_Manager::TEXTAREA,
+				'default' => esc_html__( '', 'persona-core' ),
+				'placeholder' => esc_html__( 'SVG icon code here', 'persona-core' ),
+				'label_block' => true,
+				'condition' => [
+					'persona_select_icon' => 'svg',
+				],
+			]
+		);
+
+		$this->add_control(
 			'icon_url',
 			[
 				'label' => esc_html__( 'URL', 'persona-core' ),
@@ -250,21 +191,6 @@ class Persona_Icon_box_Widget extends Widget_Base {
 				'label_block' => true,
 			]
 		);
-
-		$this->add_control(
-			'list',
-			[
-				'label' => esc_html__( 'Social List', 'persona-core' ),
-				'type' => \Elementor\Controls_Manager::REPEATER,
-				'fields' => $repeater->get_controls(),
-				'default' => [
-					[
-						'list_title' => esc_html__( 'Title #1', 'textdomain' ),
-					],
-				],
-			]
-		);
-
 
 		$this->end_controls_section();
 	}
@@ -280,74 +206,31 @@ class Persona_Icon_box_Widget extends Widget_Base {
 	 */
 	protected function render() {
 		$settings = $this->get_settings_for_display();
-		if ( ! empty( $settings['hero_button_url']['url'] ) ) {
-			$this->add_link_attributes( 'hero_button_url', $settings['hero_button_url'] );
-		}
-
 		?>
 
-         <!-- slider area start -->
-         <section class="slider__area pt-40 p-relative fix">
-            <div class="slider__item-9">
-               <div class="container">
-                  <div class="row align-items-end">
-                     <div class="col-xl-7 col-lg-6 col-md-7">
-                        <div class="slider__content-9">
-							<?php if(!empty($settings['hero_sub_title'])): ?>
-                           <span class="slider__title-pre-9"><?php echo esc_html( $settings['hero_sub_title'] ); ?></span>
-						   <?php endif; ?>
-						   <?php if(!empty($settings['hero_title'])): ?>
-                           <h3 class="slider__title-9"><?php echo wp_kses_post( $settings['hero_title'] ); ?></h3>
-						   <?php endif; ?>
-						   <?php if(!empty($settings['hero_text'])): ?>
-                           <p><?php echo esc_html( $settings['hero_text'] ); ?></p>
-						   <?php endif; ?>
-
-						   <?php if(!empty($settings['hero_button_text'])): ?>
-                           <div class="slider__btn-9 mb-85">
-                              <a <?php $this->print_render_attribute_string( 'hero_button_url' ); ?> class="tp-btn-5 tp-link-btn-3"> 
-							  <?php echo esc_html( $settings['hero_button_text'] ); ?> 
-                                 <span>
-                                    <i class="fa-regular fa-arrow-right"></i>
-                                 </span>
-                              </a>
+					<div class="services__item-9 mb-30 transition-3">
+                        <div class="services__item-9-top d-flex align-items-start justify-content-between">
+                           <div class="services__icon-9">
+                              <span>
+                                 <svg width="47" height="42" viewBox="0 0 47 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.06035 29.4262L12.5801 37.0285C17.8613 42.3677 19.6652 42.2798 24.8812 37.0285L36.9867 24.79C41.203 20.5275 42.2679 17.6931 36.9867 12.3539L29.467 4.75152C23.838 -0.939241 21.3821 0.488942 17.1659 4.75152L5.06035 16.99C-0.133935 22.2633 -0.568603 23.7354 5.06035 29.4262Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path opacity="0.4" d="M38.5975 32.1286L37.1631 34.5236C35.1419 37.9292 36.7067 40.7197 40.6405 40.7197C44.5742 40.7197 46.139 37.9292 44.1178 34.5236L42.6834 32.1286C41.5533 30.239 39.7059 30.239 38.5975 32.1286Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path opacity="0.4" d="M1.21484 22.1312C13.2986 18.8134 26.0344 18.7036 38.1616 21.8456L39.2483 22.1312" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                 </svg>  
+                                 <img src="assets/img/services/9/services-icon-shape.png" alt="">                               
+                              </span>
                            </div>
-						   <?php endif; ?>
-
-                           <div class="slider__social-9 d-flex flex-wrap align-items-center">
-                              <span>Check out my:</span>
-                              <ul>
-								<?php foreach($settings['list'] as $item): ?>
-                                 <li>
-                                    <a href="<?php echo esc_url( $item['icon_url'] ); ?>">
-									<?php \Elementor\Icons_Manager::render_icon( $item['icon'], [ 'aria-hidden' => 'true' ] ); ?>
-                                    </a>
-                                 </li>
-                                <?php endforeach; ?>
-                              </ul>
+                           <div class="services__btn-9">
+                              <a href="services-details.html"><i class="fa-light fa-arrow-up-right"></i></a>
                            </div>
                         </div>
-                     </div>
-					 <?php if(!empty($settings['hero_image']['url'])): ?>
-                     <div class="col-xl-5 col-lg-6 col-md-5 order-first order-md-last">
-                        <div class="slider__thumb-9 p-relative scene">
-                           <div class="slider__shape">
-                              <div class="slider__shape-20">
-                                 <img class="layer" data-depth=".2" src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/9/slider-shape-1.png" alt="">
-                              </div>
-                              <div class="slider__shape-21">
-                                 <img class="layer" data-depth=".3" src="<?php echo get_template_directory_uri(); ?>/assets/img/slider/9/slider-shape-2.png" alt="">
-                              </div>
-                           </div>
-                           <img class="slider__thumb-9-main" src="<?php echo esc_url( $settings['hero_image']['url'] );?>" alt="">
+                        <div class="services__content-9">
+                           <span class="services-project">86 Project</span>
+                           <h3 class="services__title-9">
+                              <a href="services-details.html">Website design</a>
+                           </h3>
                         </div>
                      </div>
-					 <?php endif; ?>
-                  </div>
-               </div>
-            </div>
-         </section>
-         <!-- slider area end -->
 
 		<?php
 	}
