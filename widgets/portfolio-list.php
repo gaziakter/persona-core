2581b4ef15d911dc3b9f7b46bc528d40e2a7d28c
Gazi Akter <gaziakterdev@gmail.com>
2024-06-28 11:08:32 +0600

    portfolio 2 design

--- widgets/portfolio-list.php
+++ widgets/portfolio-list.php
@@ -214,9 +214,9 @@ class Persona_Portfolio_list_Widget extends Widget_Base {
 	 */
 	protected function render() {
 		$settings = $this->get_settings_for_display();
-	
 		?>
 
+		<?php if($settings['design_style'] == 'style_01'): ?>
          <!-- portfolio area start -->
          <section class="portfolio__area portfolio__overlay-9 fix">
             <div class="container-fluid gx-0">
@@ -264,6 +264,264 @@ class Persona_Portfolio_list_Widget extends Widget_Base {
             </div>
          </section>
          <!-- portfolio area end -->
+		 <?php elseif($settings['design_style'] == 'style_02'): ?>
+         <!-- portfolio area start -->
+         <section class="portfolio__area pt-110 pb-70">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-12">
+                     <div class="section__title-wrapper-9 text-center mb-55">
+                        <h3 class="section__title">Latest Projects</h3>
+                     </div>
+                  </div>
+               </div>
+               <div class="row tp-gx-4">
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay=".3s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-1.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-popup">
+                              <a href="assets/img/portfolio/grid/portfolio-grid-1.jpg" class="popup-image" data-effect="mfp-zoom-in">
+                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.1667 8.33341H0.833333C0.377778 8.33341 0 7.95564 0 7.50008C0 7.04453 0.377778 6.66675 0.833333 6.66675H14.1667C14.6222 6.66675 15 7.04453 15 7.50008C15 7.95564 14.6222 8.33341 14.1667 8.33341Z" fill="currentColor"/>
+                                    <path d="M7.4974 15C7.04184 15 6.66406 14.6222 6.66406 14.1667V0.833333C6.66406 0.377778 7.04184 0 7.4974 0C7.95295 0 8.33073 0.377778 8.33073 0.833333V14.1667C8.33073 14.6222 7.95295 15 7.4974 15Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Magazine Cover</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Creative</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="portfolio-details-list.html" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay=".5s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-2.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-popup">
+                              <a href="assets/img/portfolio/grid/portfolio-grid-2.jpg" class="popup-image">
+                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.1667 8.33341H0.833333C0.377778 8.33341 0 7.95564 0 7.50008C0 7.04453 0.377778 6.66675 0.833333 6.66675H14.1667C14.6222 6.66675 15 7.04453 15 7.50008C15 7.95564 14.6222 8.33341 14.1667 8.33341Z" fill="currentColor"/>
+                                    <path d="M7.4974 15C7.04184 15 6.66406 14.6222 6.66406 14.1667V0.833333C6.66406 0.377778 7.04184 0 7.4974 0C7.95295 0 8.33073 0.377778 8.33073 0.833333V14.1667C8.33073 14.6222 7.95295 15 7.4974 15Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Logo Mockups</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Productshot</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="#" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-3.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-popup">
+                              <a href="assets/img/portfolio/grid/portfolio-grid-3.jpg" class="popup-image">
+                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.1667 8.33341H0.833333C0.377778 8.33341 0 7.95564 0 7.50008C0 7.04453 0.377778 6.66675 0.833333 6.66675H14.1667C14.6222 6.66675 15 7.04453 15 7.50008C15 7.95564 14.6222 8.33341 14.1667 8.33341Z" fill="currentColor"/>
+                                    <path d="M7.4974 15C7.04184 15 6.66406 14.6222 6.66406 14.1667V0.833333C6.66406 0.377778 7.04184 0 7.4974 0C7.95295 0 8.33073 0.377778 8.33073 0.833333V14.1667C8.33073 14.6222 7.95295 15 7.4974 15Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Pattern Inspired</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Packaging</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="portfolio-details-list.html" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay=".9s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-4.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-popup">
+                              <a href="assets/img/portfolio/grid/portfolio-grid-4.jpg" class="popup-image">
+                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.1667 8.33341H0.833333C0.377778 8.33341 0 7.95564 0 7.50008C0 7.04453 0.377778 6.66675 0.833333 6.66675H14.1667C14.6222 6.66675 15 7.04453 15 7.50008C15 7.95564 14.6222 8.33341 14.1667 8.33341Z" fill="currentColor"/>
+                                    <path d="M7.4974 15C7.04184 15 6.66406 14.6222 6.66406 14.1667V0.833333C6.66406 0.377778 7.04184 0 7.4974 0C7.95295 0 8.33073 0.377778 8.33073 0.833333V14.1667C8.33073 14.6222 7.95295 15 7.4974 15Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Product Design</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Apps</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="portfolio-details-list.html" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay="1.1s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-5.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-popup">
+                              <a href="assets/img/portfolio/grid/portfolio-grid-5.jpg" class="popup-image">
+                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.1667 8.33341H0.833333C0.377778 8.33341 0 7.95564 0 7.50008C0 7.04453 0.377778 6.66675 0.833333 6.66675H14.1667C14.6222 6.66675 15 7.04453 15 7.50008C15 7.95564 14.6222 8.33341 14.1667 8.33341Z" fill="currentColor"/>
+                                    <path d="M7.4974 15C7.04184 15 6.66406 14.6222 6.66406 14.1667V0.833333C6.66406 0.377778 7.04184 0 7.4974 0C7.95295 0 8.33073 0.377778 8.33073 0.833333V14.1667C8.33073 14.6222 7.95295 15 7.4974 15Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Branding Design</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Branding</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="portfolio-details-list.html" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-6">
+                     <div class="portfolio__grid-item mb-40 wow fadeInUp" data-wow-delay="1.3s" data-wow-duration="1s">
+                        <div class="portfolio__grid-thumb w-img fix">
+                           <a href="portfolio-details-list.html">
+                              <img src="assets/img/portfolio/grid/portfolio-grid-6.jpg" alt="">
+                           </a>
+                           <div class="portfolio__grid-video">
+                              <a href="https://www.youtube.com/watch?v=1gyTUHP6ne8" class="portfolio-play-btn popup-video">
+                                 <svg width="18" height="22" viewBox="0 0 18 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18 11L0 21.3923V0.607696L18 11Z" fill="currentColor"/>
+                                 </svg>                                    
+                              </a>
+                           </div>
+                        </div>
+                        <div class="portfolio__grid-content">
+                           <h3 class="portfolio__grid-title">
+                              <a href="portfolio-details-list.html">Original Coffee</a>
+                           </h3>
+                           <div class="portfolio__grid-bottom">
+                              <div class="portfolio__grid-category">
+                                 <span>
+                                    <a href="#">Photography</a>
+                                 </span>
+                              </div>
+                              <div class="portfolio__grid-show-project">
+                                 <a href="portfolio-details-list.html" class="portfolio-link-btn">
+                                    Show project 
+                                    <span>
+                                       <svg width="26" height="9" viewBox="0 0 26 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M21.6934 1L25 4.20003L21.6934 7.4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                          <path d="M0.999999 4.19897H25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                       </svg>
+                                    </span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!-- portfolio area end -->
+		 <?php else: ?>	
+			<h1>Design style 03</h1>					
+		 <?php endif; ?>		
 
 		<?php
 	}
