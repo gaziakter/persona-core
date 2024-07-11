036b927050c550a9ae2b71814ba20c44a3ea5fbb
Gazi Akter <gaziakterdev@gmail.com>
2024-07-11 07:08:09 +0600

    create brand widget

--- plugin.php
+++ plugin.php
@@ -11,187 +11,189 @@ use PersonaCore\PageSettings\Page_Settings;
  */
 class Persona_Core_Plugin {
 
-	/**
-	 * Instance
-	 *
-	 * @since 1.2.0
-	 * @access private
-	 * @static
-	 *
-	 * @var Plugin The single instance of the class.
-	 */
-	private static $_instance = null;
+    /**
+     * Instance
+     *
+     * @since 1.2.0
+     * @access private
+     * @static
+     *
+     * @var Plugin The single instance of the class.
+     */
+    private static $_instance = null;
 
-	/**
-	 * Instance
-	 *
-	 * Ensures only one instance of the class is loaded or can be loaded.
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 *
-	 * @return Plugin An instance of the class.
-	 */
-	public static function instance() {
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
-	}
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.2.0
+     * @access public
+     *
+     * @return Plugin An instance of the class.
+     */
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
 
-	/**
-	 * widget_scripts
-	 *
-	 * Load required plugin core files.
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 */
-	public function widget_scripts() {
-		wp_register_script( 'elementor-hello-world', plugins_url( '/assets/js/hello-world.js', __FILE__ ), [ 'jquery' ], false, true );
-	}
+    /**
+     * widget_scripts
+     *
+     * Load required plugin core files.
+     *
+     * @since 1.2.0
+     * @access public
+     */
+    public function widget_scripts() {
+        wp_register_script( 'elementor-hello-world', plugins_url( '/assets/js/hello-world.js', __FILE__ ), ['jquery'], false, true );
+    }
 
-	/**
-	 * Widgets Category
-	 *
-	 * @param [type] $elements_manager
-	 * @return void
-	 */
-	public function persona_add_elementor_widget_categories( $persona_add_cat ) {
+    /**
+     * Widgets Category
+     *
+     * @param [type] $elements_manager
+     * @return void
+     */
+    public function persona_add_elementor_widget_categories( $persona_add_cat ) {
 
-		$persona_add_cat->add_category(
-			'persona-category',
-			[
-				'title' => esc_html__( 'Persona Core', 'persona-core' ),
-				'icon' => 'fa fa-plug',
-			]
-		);	
-	}
+        $persona_add_cat->add_category(
+            'persona-category',
+            [
+                'title' => esc_html__( 'Persona Core', 'persona-core' ),
+                'icon'  => 'fa fa-plug',
+            ]
+        );
+    }
 
-	/**
-	 * Editor scripts
-	 *
-	 * Enqueue plugin javascripts integrations for Elementor editor.
-	 *
-	 * @since 1.2.1
-	 * @access public
-	 */
-	public function editor_scripts() {
-		add_filter( 'script_loader_tag', [ $this, 'editor_scripts_as_a_module' ], 10, 2 );
+    /**
+     * Editor scripts
+     *
+     * Enqueue plugin javascripts integrations for Elementor editor.
+     *
+     * @since 1.2.1
+     * @access public
+     */
+    public function editor_scripts() {
+        add_filter( 'script_loader_tag', [$this, 'editor_scripts_as_a_module'], 10, 2 );
 
-		wp_enqueue_script(
-			'elementor-hello-world-editor',
-			plugins_url( '/assets/js/editor/editor.js', __FILE__ ),
-			[
-				'elementor-editor',
-			],
-			'1.2.1',
-			true
-		);
-	}
+        wp_enqueue_script(
+            'elementor-hello-world-editor',
+            plugins_url( '/assets/js/editor/editor.js', __FILE__ ),
+            [
+                'elementor-editor',
+            ],
+            '1.2.1',
+            true
+        );
+    }
 
-	/**
-	 * Force load editor script as a module
-	 *
-	 * @since 1.2.1
-	 *
-	 * @param string $tag
-	 * @param string $handle
-	 *
-	 * @return string
-	 */
-	public function editor_scripts_as_a_module( $tag, $handle ) {
-		if ( 'elementor-hello-world-editor' === $handle ) {
-			$tag = str_replace( '<script', '<script type="module"', $tag );
-		}
+    /**
+     * Force load editor script as a module
+     *
+     * @since 1.2.1
+     *
+     * @param string $tag
+     * @param string $handle
+     *
+     * @return string
+     */
+    public function editor_scripts_as_a_module( $tag, $handle ) {
+        if ( 'elementor-hello-world-editor' === $handle ) {
+            $tag = str_replace( '<script', '<script type="module"', $tag );
+        }
 
-		return $tag;
-	}
+        return $tag;
+    }
 
-	/**
-	 * Register Widgets
-	 *
-	 * Register new Elementor widgets.
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 *
-	 * @param Widgets_Manager $widgets_manager Elementor widgets manager.
-	 */
-	public function register_widgets( $widgets_manager ) {
-		// Its is now safe to include Widgets files
-		require_once( __DIR__ . '/widgets/inline-editing.php' );
-		require_once( __DIR__ . '/widgets/hero.php' );
-		require_once( __DIR__ . '/widgets/about-me.php' );
-		require_once( __DIR__ . '/widgets/process.php' );
-		require_once( __DIR__ . '/widgets/icon-box.php' );
-		require_once( __DIR__ . '/widgets/heading.php' );
-		require_once( __DIR__ . '/widgets/heading.php' );
-		require_once( __DIR__ . '/widgets/button.php' );
-		require_once( __DIR__ . '/widgets/portfolio-list.php' );
-		require_once( __DIR__ . '/widgets/skill.php' );
-		require_once( __DIR__ . '/widgets/exprience.php' );
-		require_once( __DIR__ . '/widgets/awards.php' );
-		require_once( __DIR__ . '/widgets/testimonial.php' );
-		require_once( __DIR__ . '/widgets/blog-post.php' );
-		require_once( __DIR__ . '/widgets/contact-info.php' );
-		require_once( __DIR__ . '/widgets/about-bg.php' );
+    /**
+     * Register Widgets
+     *
+     * Register new Elementor widgets.
+     *
+     * @since 1.2.0
+     * @access public
+     *
+     * @param Widgets_Manager $widgets_manager Elementor widgets manager.
+     */
+    public function register_widgets( $widgets_manager ) {
+        // Its is now safe to include Widgets files
+        require_once __DIR__ . '/widgets/inline-editing.php';
+        require_once __DIR__ . '/widgets/hero.php';
+        require_once __DIR__ . '/widgets/about-me.php';
+        require_once __DIR__ . '/widgets/process.php';
+        require_once __DIR__ . '/widgets/icon-box.php';
+        require_once __DIR__ . '/widgets/heading.php';
+        require_once __DIR__ . '/widgets/heading.php';
+        require_once __DIR__ . '/widgets/button.php';
+        require_once __DIR__ . '/widgets/portfolio-list.php';
+        require_once __DIR__ . '/widgets/skill.php';
+        require_once __DIR__ . '/widgets/exprience.php';
+        require_once __DIR__ . '/widgets/awards.php';
+        require_once __DIR__ . '/widgets/testimonial.php';
+        require_once __DIR__ . '/widgets/blog-post.php';
+        require_once __DIR__ . '/widgets/contact-info.php';
+        require_once __DIR__ . '/widgets/about-bg.php';
+        require_once __DIR__ . '/widgets/brand.php';
 
-		// Register Widgets
-		$widgets_manager->register( new Widgets\Persona_Hero_Widget() );
-		$widgets_manager->register( new Widgets\Persona_About_me_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Processs_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Icon_box_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Heading_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Button_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Portfolio_list_Widget() );
-		$widgets_manager->register( new Widgets\Inline_Editing() );
-		$widgets_manager->register( new Widgets\Persona_skill_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Exprience_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Awards_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Testimonial_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Blog_post_Widget() );
-		$widgets_manager->register( new Widgets\Persona_Contact_Info_Widget() );
-		$widgets_manager->register( new Widgets\Persona_About_Bg_Widget() );
-	}
+        // Register Widgets
+        $widgets_manager->register( new Widgets\Persona_Hero_Widget() );
+        $widgets_manager->register( new Widgets\Persona_About_me_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Processs_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Icon_box_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Heading_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Button_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Portfolio_list_Widget() );
+        $widgets_manager->register( new Widgets\Inline_Editing() );
+        $widgets_manager->register( new Widgets\Persona_skill_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Exprience_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Awards_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Testimonial_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Blog_post_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Contact_Info_Widget() );
+        $widgets_manager->register( new Widgets\Persona_About_Bg_Widget() );
+        $widgets_manager->register( new Widgets\Persona_Brand_Widget() );
+    }
 
-	/**
-	 * Add page settings controls
-	 *
-	 * Register new settings for a document page settings.
-	 *
-	 * @since 1.2.1
-	 * @access private
-	 */
-	private function add_page_settings_controls() {
-		require_once( __DIR__ . '/page-settings/manager.php' );
-		new Page_Settings();
-	}
+    /**
+     * Add page settings controls
+     *
+     * Register new settings for a document page settings.
+     *
+     * @since 1.2.1
+     * @access private
+     */
+    private function add_page_settings_controls() {
+        require_once __DIR__ . '/page-settings/manager.php';
+        new Page_Settings();
+    }
 
-	/**
-	 *  Plugin class constructor
-	 *
-	 * Register plugin action hooks and filters
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 */
-	public function __construct() {
+    /**
+     *  Plugin class constructor
+     *
+     * Register plugin action hooks and filters
+     *
+     * @since 1.2.0
+     * @access public
+     */
+    public function __construct() {
 
-		// Add New Category
-		add_action( 'elementor/elements/categories_registered',  [ $this, 'persona_add_elementor_widget_categories' ] );
+        // Add New Category
+        add_action( 'elementor/elements/categories_registered', [$this, 'persona_add_elementor_widget_categories'] );
 
-		// Register widget scripts
-		add_action( 'elementor/frontend/after_register_scripts', [ $this, 'widget_scripts' ] );
+        // Register widget scripts
+        add_action( 'elementor/frontend/after_register_scripts', [$this, 'widget_scripts'] );
 
-		// Register widgets
-		add_action( 'elementor/widgets/register', [ $this, 'register_widgets' ] );
+        // Register widgets
+        add_action( 'elementor/widgets/register', [$this, 'register_widgets'] );
 
-		// Register editor scripts
-		add_action( 'elementor/editor/after_enqueue_scripts', [ $this, 'editor_scripts' ] );
-		
-		$this->add_page_settings_controls();
-	}
+        // Register editor scripts
+        add_action( 'elementor/editor/after_enqueue_scripts', [$this, 'editor_scripts'] );
+
+        $this->add_page_settings_controls();
+    }
 }
 
 // Instantiate Plugin Class
---
+++ widgets/brand.php
@@ -0,0 +1,257 @@
+<?php
+namespace PersonaCore\Widgets;
+
+use Elementor\Widget_Base;
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+// Exit if accessed directly
+
+/**
+ * Elementor Hello World
+ *
+ * Elementor widget for hello world.
+ *
+ * @since 1.0.0
+ */
+class Persona_Brand_Widget extends Widget_Base {
+
+    /**
+     * Retrieve the widget name.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     *
+     * @return string Widget name.
+     */
+    public function get_name() {
+        return 'persona-brand-widget';
+    }
+
+    /**
+     * Retrieve the widget title.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     *
+     * @return string Widget title.
+     */
+    public function get_title() {
+        return __( 'Persona Brands', 'persona' );
+    }
+
+    /**
+     * Retrieve the widget icon.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     *
+     * @return string Widget icon.
+     */
+    public function get_icon() {
+        return 'eicon-posts-ticker';
+    }
+
+    /**
+     * Retrieve the list of categories the widget belongs to.
+     *
+     * Used to determine where to display the widget in the editor.
+     *
+     * Note that currently Elementor supports only one category.
+     * When multiple categories passed, Elementor uses the first one.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     *
+     * @return array Widget categories.
+     */
+    public function get_categories() {
+        return ['persona-category'];
+    }
+
+    /**
+     * Retrieve the list of scripts the widget depended on.
+     *
+     * Used to set scripts dependencies required to run the widget.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     *
+     * @return array Widget scripts dependencies.
+     */
+    public function get_script_depends() {
+        return ['elementor-hello-world'];
+    }
+
+    /**
+     * Register the widget controls.
+     *
+     * Adds different input fields to allow the user to change and customize the widget settings.
+     *
+     * @since 1.0.0
+     *
+     * @access protected
+     */
+    protected function register_controls() {
+
+        /** Process Section */
+        $this->start_controls_section(
+            'persoan_exprience_section',
+            [
+                'label' => esc_html__( 'Persona Exprience List', 'persona-core' ),
+                'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $repeater = new \Elementor\Repeater();
+
+        $repeater->add_control(
+            'item_title',
+            [
+                'label'       => esc_html__( 'Award Title', 'persona-core' ),
+                'type'        => \Elementor\Controls_Manager::TEXT,
+                'default'     => esc_html__( 'Modern painting award jump', 'persona-core' ),
+                'label_block' => true,
+            ]
+        );
+
+        $repeater->add_control(
+            'item_sub_title',
+            [
+                'label'       => esc_html__( 'Sub Title', 'persona-core' ),
+                'type'        => \Elementor\Controls_Manager::TEXT,
+                'default'     => esc_html__( 'Runner Up - Decor of the week', 'persona-core' ),
+                'label_block' => true,
+            ]
+        );
+
+        $repeater->add_control(
+            'item_image',
+            [
+                'label'   => esc_html__( 'Choose Image', 'persona-core' ),
+                'type'    => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src(),
+                ],
+            ]
+        );
+
+        $repeater->add_control(
+            'item_url',
+            [
+                'label'       => esc_html__( 'URL', 'persona-core' ),
+                'type'        => \Elementor\Controls_Manager::TEXT,
+                'default'     => esc_html__( '#', 'persona-core' ),
+                'label_block' => true,
+            ]
+        );
+
+        $repeater->add_control(
+            'item_duration',
+            [
+                'label'       => esc_html__( 'Award year', 'persona-core' ),
+                'type'        => \Elementor\Controls_Manager::TEXT,
+                'default'     => esc_html__( 'Art direction 2021', 'persona-core' ),
+                'label_block' => true,
+            ]
+        );
+
+        $this->add_control(
+            'persona_item_list',
+            [
+                'label'   => esc_html__( 'Portfolio List', 'persona-core' ),
+                'type'    => \Elementor\Controls_Manager::REPEATER,
+                'fields'  => $repeater->get_controls(),
+                'default' => [
+                    [
+                        'process_title'   => esc_html__( 'Title #1', 'persona-core' ),
+                        'process_content' => esc_html__( 'Title #1', 'persona-core' ),
+                    ],
+                    [
+                        'process_title'   => esc_html__( 'Title #1', 'persona-core' ),
+                        'process_content' => esc_html__( 'Title #1', 'persona-core' ),
+                    ],
+                    'title_field' => '{{{process_title}}}',
+                ],
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    /**
+     * Render the widget output on the frontend.
+     *
+     * Written in PHP and used to generate the final HTML.
+     *
+     * @since 1.0.0
+     *
+     * @access protected
+     */
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+        if ( !empty( $settings['item_url']['url'] ) ) {
+            $this->add_link_attributes( 'item_url', $settings['item_url'] );
+        }
+        ?>
+                    <div class="award__item-wrapper-9">
+                    <?php foreach ( $settings['persona_item_list'] as $key => $item ): ?>
+                        <div class="award__item-9 p-relative wow fadeInUp" data-wow-delay=".3s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;">
+                           <div class="row align-items-center">
+                              <div class="col-xl-3 col-lg-3 col-md-3">
+                                 <div class="award__topic">
+                                    <p><?php echo esc_html( $item['item_duration'] ); ?></p>
+                                 </div>
+                              </div>
+                              <div class="col-xl-7 col-lg-7 col-md-7">
+                                 <div class="award__content-9">
+                                    <h3 class="award__title-9">
+                                       <a href="<?php echo esc_url( $item['item_url'] ); ?>" class="tp-img-reveal tp-img-reveal-item" data-img="<?php echo esc_url( $item['item_image']['url'] ); ?>" data-fx="1" style=""><?php echo esc_html( $item['item_title'] ); ?><div class="tp-img-reveal-wrapper" style="top: 331px; left: 1030px; opacity: 0;"><div class="tp-img-reveal-wrapper__inner" style="overflow: hidden; transform: translate(100%, 0%) matrix(1, 0, 0, 1, 0, 0);">
+                <div class="tp-img-reveal-wrapper__img" style="background-image: url(<?php echo esc_url( $item['item_image']['url'] ); ?>); transform: translate(-100%, 0%) matrix(1, 0, 0, 1, 0, 0);">
+                    <div class="tp-hover-wrapper">
+                        <span class="tp-hover-subtitle"></span>
+                        <h3 class="tp-hover-title"></h3>
+                        <p></p>
+                        <div class="tp-hover-meta">
+                            <span>
+                                <a href="#"></a>
+                            </span>
+                            <span>
+                                <a href="#"></a>
+                            </span>
+                            <span>
+                                <a href="#"></a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div></div></a>
+                                    </h3>
+                                    <p><?php echo esc_html( $item['item_sub_title'] ); ?></p>
+                                 </div>
+                              </div>
+                              <div class="col-xl-2 col-lg-2 col-md-2">
+                                 <div class="award__btn-9 text-md-end">
+                                    <a href="<?php echo esc_url( $item['item_url'] ); ?>" class="career-link-btn">
+                                       <svg width="22" height="18" viewBox="0 0 22 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M12.7334 1L21 9.00007L12.7334 17" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                          <path d="M0.999999 8.99756H21" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                                       </svg>
+                                    </a>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                    <?php endforeach;?>
+
+                     </div>
+
+		<?php
+}
+}
